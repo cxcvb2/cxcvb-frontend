1e09e2c05389d1da5de79c8ccf1e7e0eed2a8efd
VaruZHV <varuzhan.g1.y@tumo.com>
2021-07-28 20:42:39 +0300

    chnage design and add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import '../styles/globals.css'
 import MainLayout from '../components/Layout/MainLayout'
 import { IntlProvider } from 'react-intl'
-
+import Router from 'next/router'
 import useLang from '../content/locale'
+import React from 'react'
+import Loader from '../components/auxiliary-elements/Loader/Loader'
 
 function MyApp({ Component, pageProps }) {
   const { messages, locale, defaultLocale } = useLang()
+  const [loading, setLoading] = React.useState(false)
+  React.useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setLoading(true)
+    }
+    const end = () => {
+      console.log('findished')
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
   return (
     <IntlProvider
       locale={locale}
@@ -13,7 +34,7 @@ function MyApp({ Component, pageProps }) {
       messages={messages}
     >
       <MainLayout>
-        <Component {...pageProps} />
+        {loading && <Loader />} <Component {...pageProps} />
       </MainLayout>
     </IntlProvider>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-
+    console.log(ctx)
     return {
       ...initialProps,
     }
