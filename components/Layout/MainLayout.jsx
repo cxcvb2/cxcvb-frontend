ddc88cf70430f6a0de5a194c80e64b11f6872794
VaruZHV <varuzhan.g1.y@tumo.com>
2021-08-02 14:57:37 +0300

    do later skeleton

--- components/Layout/MainLayout.jsx
+++ components/Layout/MainLayout.jsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Router from 'next/router'
 import React from 'react'
+import Loader from '../auxiliary-elements/Loader/Loader'
 import { IntlProvider } from 'react-intl'
 import useLang from '../../content/locale'
 import Header from '../Header/Header'
@@ -8,6 +10,25 @@ import Footer from '../Footer/Footer'
 export default function MainLayout({ children }) {
   const { messages, locale, defaultLocale } = useLang()
 
+  const [loading, setLoading] = React.useState(false)
+  React.useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setLoading(true)
+    }
+    const end = () => {
+      console.log('findished')
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
   return (
     <>
       <Head>
@@ -25,7 +46,7 @@ export default function MainLayout({ children }) {
         messages={messages}
       >
         <Header />
-        {children}
+        {loading && <Loader />} {children}
         <Footer />
       </IntlProvider>
     </>
